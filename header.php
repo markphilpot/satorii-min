325f278f6017fb7eb9f7cd6524eb909cc6a45646
Mark Philpot <mark.philpot@gmail.com>
2011-01-26 17:04:30 +0000

    Font, style and js fixes

--- header.php
+++ header.php
@@ -3,15 +3,16 @@
 <head profile="http://gmpg.org/xfn/11">
 	<title><?php wp_title( '-', true, 'right' ); echo wp_specialchars( get_bloginfo('name'), 1 ) ?></title>
 	<meta http-equiv="content-type" content="<?php bloginfo('html_type') ?>; charset=<?php bloginfo('charset') ?>" />
+        <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory') ?>/fonts/LiberationSans/stylesheet.css" />
+        <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory') ?>/fonts/CartoGothic/stylesheet.css" />
 	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url') ?>" />
 	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory') ?>/css/fancy.yuic.css" />		
 	
 	<link rel="alternate" type="application/rss+xml" href="<?php bloginfo('rss2_url') ?>" title="<?php printf( __( '%s latest posts', 'sandbox' ), wp_specialchars( get_bloginfo('name'), 1 ) ) ?>" />
 	<link rel="alternate" type="application/rss+xml" href="<?php bloginfo('comments_rss2_url') ?>" title="<?php printf( __( '%s latest comments', 'sandbox' ), wp_specialchars( get_bloginfo('name'), 1 ) ) ?>" />
 	<link rel="pingback" href="<?php bloginfo('pingback_url') ?>" />
-	
-	<script type="text/javascript" src="<?php bloginfo('template_directory')?>/js/jquery.min.js"></script>
-	<script type="text/javascript" src="<?php bloginfo('template_directory')?>/js/jquery.fancybox.yuic.js"></script>
+
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.js"></script>
 	<!--[if lt IE 7]><script src="<?php bloginfo('template_directory')?>/js/jquery.pngFix.pack.js" type="text/javascript"></script><![endif]-->
 	<script type="text/javascript" src="<?php bloginfo('template_directory')?>/js/satorii.js"></script>
 	
--- js/satorii.js
+++ js/satorii.js
@@ -1,23 +1,9 @@
 $(document).ready(function() {
-	
-	$('div#content div[id^=post]').each(function() { 
-		var postId = $(this).attr('id');
-		$('div#'+postId+' a[href$=".png"]').addClass('fancybox').attr('rel', postId);
-		$('div#'+postId+' a[href$=".gif"]').addClass('fancybox').attr('rel', postId);
-		$('div#'+postId+' a[href$=".jpg"]').addClass('fancybox').attr('rel', postId);
-		$('div#'+postId+' a[href$=".PNG"]').addClass('fancybox').attr('rel', postId);
-		$('div#'+postId+' a[href$=".GIF"]').addClass('fancybox').attr('rel', postId);
-		$('div#'+postId+' a[href$=".JPG"]').addClass('fancybox').attr('rel', postId)
-	});
-	
-	$('a.fancybox').fancybox({
-		'overlayShow' : true,
-		'hideOnContentClick': true
-		});
-		
+
 	$('table tr').removeClass('odd even');
 	$('table tr:even').addClass('odd');
-		
+	
+	
 	$('#comments-list .comment-meta').append(' <span class="meta-sep">|</span> <a class="reply" href="#reply">Reply</a>');
 	
 	$('.reply').click(function() {
